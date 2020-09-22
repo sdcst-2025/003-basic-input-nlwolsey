615d3af53e87e461c38a8f78a5139cd8f9d28638
Chris Yang <cyang@deltalearns.ca>
2020-09-22 14:42:27 -0700

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,15 @@
+#! python3
+#
+# Find the hypotenuse
+# Your program will ask the user to enter in the 2 short sides of a right triangle.
+# You will calculate the length of the hypotenuse and display the result.
+# You will need to use the math module to use the command that finds the square root.
+#
+# Inputs:
+# side, side
+#
+# Outputs:
+# hypotenuse
+#
+# Test output
+# input sides of 5 and 7 should give hypotenuse of 8.60232526704
