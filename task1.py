d46fc449c12a473df8bebccb129b980610aed932
Chris Yang <cyang@deltalearns.ca>
2020-09-22 14:33:57 -0700

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,11 @@
+#! python3
+# Ask the user for their name and their email address.
+# (2 points)
+#
+# Inputs:
+# name
+# email
+#
+# Sample output:
+# Your name is Joe Lunchbox, and your email is joe@koolsandwiches.org.
+
