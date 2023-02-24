50ded7eb036a47d9a746bf49cecdbbb34eef6577
Chris Yang <cyang@deltalearns.ca>
2023-02-24 11:08:44 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -9,4 +9,16 @@ Inputs:
 
 Sample output:
  Your name is Joe Lunchbox, and your email is joe@koolsandwiches.org.
+
+example:
+What is your name:Dwayne Johnson
+What ir your email:rock@aol.com
+
+Your name is Dwayne Johnson, and your email is rock@aol.com
+
+What is your name:Jackie Chan
+What ir your email:crazyAsian@qq.com
+
+Your name is Jackie Chan, and your email is crazyAsian@qq.com
+
 """
